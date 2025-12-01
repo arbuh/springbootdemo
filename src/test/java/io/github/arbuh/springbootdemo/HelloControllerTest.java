2af70871fd09ed9f1e815bcaa05d5b480f81b4f8
pavels <p.smirnov@outlook.com>
2025-12-01 15:19:03 +0100

    Implement test endpoint

---
+++ src/test/java/io/github/arbuh/springbootdemo/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package io.github.arbuh.springbootdemo;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getIndex() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+}
